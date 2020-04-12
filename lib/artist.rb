2c2e964ee92b51fc6047c478f8a1817438c6463f
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-04-12 04:30:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,25 @@
-class Artist 
-  
-   @@all = []
-   
+class Artist
   attr_accessor :name
-#The Artist class needs a class variable @@all that begins as an empty array
+  @@all = []
 
-  def initialize(name)
+  def initialize name
     @name = name
     @@all << self
   end
-  #The Artist class needs a class method .all that lists each artist in the class variable
-  
+
   def songs
-    Song.all.select {|song| song.artist == self}
+    Song.all.select {|song| song.artist == self }
   end
-  
+
+  def new_song name, genre
+    song = Song.new(name, self, genre)
+  end
+
   def genres
-    songs.map {|song| song.genre}
+    self.songs.map {|song| song.genre }
   end
-  
+
   def self.all
     @@all
   end
-  
-  def new_song(song, genre)
-    Song.new(name, self, genre)
-  end
-  
-
 end
